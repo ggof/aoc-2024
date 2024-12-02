b148b4f3eab5acb199586bd0f83446062d92b928
Gabriel Goffart <ggoffart@genetec.com>
2024-12-02 14:29:34 -0500

    refactor

--- src/aoc_2024/day02/main.clj
+++ src/aoc_2024/day02/main.clj
@@ -1,10 +1,6 @@
 (ns aoc-2024.day02.main
   (:require [clojure.string :as str]))
 
-(defn tee [x]
-  (println x)
-  x)
-
 (defn ordered? [coll]
   (or (apply < coll) (apply > coll)))
 
@@ -22,21 +18,12 @@
         coll (map #(map Integer/parseInt %) coll)]
     coll))
 
-(defn sum-safes [coll]
+(defn part-1 [coll]
   (->> coll
        to-int-lists
        (filter ordered?)
        (filter safe?)
        count))
 
-(def var1 "7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9")
-
 (defn main [_]
-  (let [input  (slurp "inputs/day02.txt")]
-    (println (sum-safes (str/split-lines input)))))
-
+  (println (part-1 (str/split-lines (slurp "inputs/day02.txt")))))
