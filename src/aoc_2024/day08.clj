fe713d0dd2f6923d0400a08cfd4f48a2bae64e7e
Gabriel Goffart <gab.goffart@gmail.com>
2024-12-12 21:03:09 -0500

    add day08 part2

--- src/aoc_2024/day08.clj
+++ src/aoc_2024/day08.clj
@@ -2,35 +2,54 @@
   (:require
    [aoc-2024.lib :as lib]))
 
-(defn in-bounds? [max-x max-y [x y]] (and (<= 0 x) (< x max-x) (<= 0 y) (< y max-y)))
+(def max-x 50)
+(def max-y 50)
 
-(defn get-antinodes [max-x max-y c a]
+(defn in-bounds? [[x y]] (and (<= 0 x) (< x max-x) (<= 0 y) (< y max-y)))
+
+(defn get-first-antinodes [c a]
   (let [c-a (lib/sub-vec c a)
         a-c (lib/sub-vec a c)
         p1 (lib/add-vec c c-a)
         p2 (lib/add-vec a a-c)]
-    (filter (partial in-bounds? max-x max-y) [p1 p2])))
+    (filter in-bounds? [p1 p2])))
 
-(defn antinodes-for [pos antennas nodes max-x max-y]
-  (let [new-nodes (transduce (map (partial get-antinodes max-x max-y pos)) concat antennas)]
+(defn antinodes-for [get-antinodes pos antennas nodes]
+  (let [new-nodes (transduce (map (partial get-antinodes pos)) concat antennas)]
     (apply conj nodes new-nodes)))
 
-(defn solve [chars max-x max-y]
+(defn solve [get-antinodes chars]
   (loop [[hd & tl] chars
          ants {}
          nodes #{}
          [x y] [0 0]]
     (cond
-      (empty? tl) (count (antinodes-for [x y] (ants hd) nodes max-x max-y))
+      (empty? tl) (count (antinodes-for get-antinodes [x y] (ants hd) nodes))
       (= \newline hd) (recur tl ants nodes [0 (inc y)])
       (= \. hd) (recur tl ants nodes [(inc x) y])
       :else (let [antennas (ants hd)
-                  new-nodes (antinodes-for [x y] antennas nodes max-x max-y)]
+                  new-nodes (antinodes-for get-antinodes [x y] antennas nodes)]
               (recur tl (update ants hd conj [x y]) new-nodes [(inc x) y])))))
 
-(defn day-1 [input]
-  (solve (seq input) 50 50))
+(defn part-1 [input]
+  (solve get-first-antinodes (seq input)))
+
+(defn in-direction [p d]
+  (loop [acc []
+         pos p]
+    (if (in-bounds? pos)
+      (recur (conj acc pos) (lib/add-vec pos d))
+      acc)))
+
+(defn get-all-antinodes [c a]
+  (let [c-a (lib/sub-vec c a)
+        a-c (lib/sub-vec a c)]
+    (concat (in-direction c a-c) (in-direction a c-a))))
+
+(defn part-2 [input]
+  (solve get-all-antinodes (seq input)))
 
 (defn main [_]
   (let [input (slurp "inputs/day08.txt")]
-    (println (day-1 input))))
+    (println (part-1 input))
+    (println (part-2 input))))
