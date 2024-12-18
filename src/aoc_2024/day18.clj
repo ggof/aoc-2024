d1f61bf1751f57fbc8af833a676303b1d355a4b6
Gabriel Goffart <ggoffart@genetec.com>
2024-12-18 15:23:36 -0500

    still working on day 18, bfs doesnt work for some reason

--- src/aoc_2024/day18.clj
+++ src/aoc_2024/day18.clj
@@ -3,29 +3,26 @@
    [aoc-2024.lib :as lib]
    [clojure.string :as str]))
 
-(defn find-neighbors [v g] (g v))
-
-(defn visited? [v coll] (some #(= % v) coll))
-
 (defn bfs
-  "Traverses a graph in Breadth First Search (BFS)."
-  [graph v]
-  (loop [queue   (conj clojure.lang.PersistentQueue/EMPTY v) ;; Use a queue to store the nodes we need to explore
-         visited []]                                         ;; A vector to store the sequence of visited nodes
-    (if (empty? queue) visited                               ;; Base case - return visited nodes if the queue is empty
-        (let [v           (peek queue)
-              neighbors   (find-neighbors v graph)
-              not-visited (filter (complement #(visited? % visited)) neighbors)
-              new-queue   (apply conj (pop queue) not-visited)]
-          (if (visited? v visited)
-            (recur new-queue visited)
-            (recur new-queue (conj visited v)))))))
+  [graph s e]
+  (loop [queue   [[s]]
+         visited #{}]
+    (let [path        (first queue)
+          v           (first path)
+          neighbors   (graph v)
+          not-visited (filter (complement visited) neighbors)
+          next-paths  (map #(cons % path) not-visited)
+          new-queue   (apply conj (rest queue) next-paths)]
+      (cond
+        (= e v) path
+        (visited v) (recur (rest queue) visited)
+        :else (recur new-queue (conj visited v))))))
 
 (defn in-grid? [size [x y]]
   (and (< x size) (< y size) (>= x 0) (>= y 0)))
 
 (defn surroundings [size pos walls]
-  (set (filter (every-pred (partial in-grid? size) #(not (walls %))) (map (partial lib/add-vec pos) [[-1 0] [0 -1] [1 0] [0 1]]))))
+  (set (filter (every-pred (partial in-grid? size) (complement walls)) (map (partial lib/add-vec pos) [[-1 0] [0 -1] [1 0] [0 1]]))))
 
 (defn create-grid [size walls]
   (into {} (for [y (range size)
@@ -33,34 +30,25 @@
                  :when (not (walls [x y]))]
              [[x y] (surroundings size [x y] walls)])))
 
-(def input "5,4
-4,2
-4,5
-3,0
-2,1
-6,3
-2,4
-1,5
-0,6
-3,3
-2,6
-5,1
-1,2
-5,5
-2,5
-6,5
-1,4
-0,4
-6,4
-1,1
-6,1
-1,0
-0,5
-1,6
-2,0")
+(defn parse [input nb-bytes]
+  (into #{} (take nb-bytes (map #(map parse-long %) (map #(str/split % #",") (str/split-lines input))))))
+
+; (defn write-mase [grid]
+;   (spit "day18.txt" (str/join (for [y (range 71)
+;                                     x (range 71)]
+;                                 (cond
+;                                   (and (= x 70) (grid [x y])) ".\n"
+;                                   (grid [x y]) "."
+;                                   (= x 70) "#\n"
+;                                   :else "#")))))
+(defn part-1 [input]
+  (let [walls (parse input 1024)
+        grid (create-grid 71 walls)
+        path (bfs grid [0 0] [70 70])]
+
+    ; (write-mase grid)
+    (- (count path) 1)))
 
 (defn main [_]
-  (let [walls (into #{} (take 12 (map #(map parse-long %) (map #(str/split % #",") (str/split-lines input)))))
-        walls (apply assoc (map #(list % 1) walls))
-        grid (create-grid 7 walls)]
-    (dijkstra grid [0 0] :target [6 6])))
+  (let [input (slurp "inputs/day18.txt")]
+    (println (part-1 input))))
